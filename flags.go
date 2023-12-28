0c9c0785c013e4413d30647c997a99e3a1a13af4
Chris <github.account@chrigel.net>
2023-12-28 17:08:05 +0100

    Initial bootstrap with pterm & cli

---
+++ flags.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+func newLogLevelFlag() *cli.StringFlag {
+	return &cli.StringFlag{
+		Name: "log-level", Aliases: []string{"v"}, EnvVars: []string{"LOG_LEVEL"},
+		Usage:       "logging verbosity",
+		DefaultText: "info [debug | warn | error | disabled]",
+		Value:       "info",
+	}
+}
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/pterm/pterm"
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	// These will be populated by Goreleaser
+	version = "unknown"
+	commit  = "-dirty-"
+	date    = time.Now().Format("2006-01-02")
+
+	appName     = "web-powercycle"
+	appLongName = "Shut down Linux over web interface"
+)
+
+func main() {
+	app := NewApp()
+	err := app.Run(os.Args)
+	if err != nil {
+		pterm.DefaultLogger.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func NewApp() *cli.App {
+	app := &cli.App{
+		Name:    appName,
+		Usage:   appLongName,
+		Version: fmt.Sprintf("%s, revision=%s, date=%s", version, commit, date),
+
+		Before: before(setupLogging),
+		Flags: []cli.Flag{
+			newLogLevelFlag(),
+		},
+		Action: actions(LogMetadata),
+	}
+	return app
+}
+
+func before(actions ...cli.BeforeFunc) cli.BeforeFunc {
+	return func(ctx *cli.Context) error {
+		for _, fn := range actions {
+			if err := fn(ctx); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func actions(actions ...cli.ActionFunc) cli.ActionFunc {
+	return func(ctx *cli.Context) error {
+		for _, action := range actions {
+			if err := action(ctx); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
